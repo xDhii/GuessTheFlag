a28ab4c3466fecbe8fa059510d4ee033fcfe2433
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-12 11:17:26 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Adriano Valumin on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
